8440c514b2baaf6e94ff0f89f5e1ae3260f3e34f
Abdeslem Ziaissa <aziaissa@hotmail.fr>
2021-12-11 22:44:12 +0100

    Update db.config.js
    Update db.config.js

--- node-js-server/app/config/db.config.js
+++ node-js-server/app/config/db.config.js
@@ -1,16 +1,16 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "Abdes",
-  PASSWORD: "123456",
-  DB: "testdb",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "Abdes",
+//   PASSWORD: "123456",
+//   DB: "testdb",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
 
 exports.PRODUCTION = {
   HOST: "/cloudsql/peak-service-334315:us-central1:testdb",
