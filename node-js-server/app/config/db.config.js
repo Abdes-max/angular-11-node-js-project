8dafeec640670f97df7e09c935547f6203efc041
Abdeslem Ziaissa <aziaissa@hotmail.fr>
2021-12-11 22:30:58 +0100

    Accrochage cloud SQL
    Accrochage cloud SQL

--- node-js-server/app/config/db.config.js
+++ node-js-server/app/config/db.config.js
@@ -11,3 +11,17 @@ module.exports = {
     idle: 10000
   }
 };
+
+exports.PRODUCTION = {
+  HOST: "/cloudsql/peak-service-334315:us-central1:testdb",
+  USER: "Abdes",
+  PASSWORD: "123456",
+  DB: "testdb",
+  dialect: "mysql",
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  }
+};
