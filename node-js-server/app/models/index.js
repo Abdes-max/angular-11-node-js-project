34c5c371dcffd69795ed1a589cdcdee5c13640c9
Abdeslem Ziaissa <aziaissa@hotmail.fr>
2021-12-15 21:53:02 +0100

    Config app engine

--- node-js-server/app/models/index.js
+++ node-js-server/app/models/index.js
@@ -1,11 +1,25 @@
 const dbConfig = require("../config/db.config.js");
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_INSTANCE = process.env.DB_INSTANCE;
+
+const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//   host: dbConfig.HOST,
+//   dialect: dbConfig.dialect,
+//   dialectOptions: {
+//     socketPath: dbConfig.HOST,
+//   },
+// });
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: `/cloudsql/${DB_INSTANCE}`,
+  dialect: 'mysql',
   dialectOptions: {
-    socketPath: dbConfig.HOST,
+    socketPath: `/cloudsql/${DB_INSTANCE}`,
   },
 });
 
